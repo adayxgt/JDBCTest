86ad75f9572bb8a70e4fc3132f46cf0943f99933
adayxgt <adayxgt@gmail.com>
2025-09-25 13:43:17 +0200

    jdbctest

--- src/test/UserDao.java
+++ src/test/UserDao.java
@@ -1,5 +1,25 @@
 package test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * data access object for user table
+ */
+
 public class UserDao {
 
+	public void insert(int id, String name, String psw, boolean isVIp) {
+		String insertSQL = 
+				"insert into users values(1, 'Aday', '1234', 1, )";
+		
+		Connection connection = DBHelper.getConnection();
+		try {
+		PreparedStatement ps = connection .prepareStatement(insertSQL);
+		ps.executeUpdate();
+		
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
 }
